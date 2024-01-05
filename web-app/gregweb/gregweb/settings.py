56a2bb9c6f776ad7bdc76d1a7e6d8bf654e6598c
GregRublik <150723587+GregRublik@users.noreply.github.com>
2024-01-05 22:17:14 +0300

    Update settings.py

--- web-app/gregweb/gregweb/settings.py
+++ web-app/gregweb/gregweb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%91*9!hib07ses&j8o1!d$ux**8+$41e9w@o-nwux^plv$$pn_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
